f2921d95459f1ea83a91c11c6abd455d3f53138b
Тажиев Алмас <almas200224@gmail.com>
2024-05-26 22:47:25 +0600

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import React from "react";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react()],
   base: "/<REPO>/",
 });
